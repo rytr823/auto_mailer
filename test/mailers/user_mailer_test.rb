46d3bba3df9d13f692a97bcc31cda3c00f83af80
rytr823 <rytr823@gmail.com>
2020-07-19 13:26:59 +0900

    Add mailer test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,11 +1,16 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  test 'trends' do
-    mail = UserMailer.trends
+  test 'trends(user)' do
+    email = UserMailer.trends
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
     assert_equal 'Trends', mail.subject
-    assert_equal ['to@example.org'], mail.to
-    assert_equal ['from@example.com'], mail.from
-    assert_match 'Hi', mail.body.encoded
+    assert_equal ['rh_mc_bb@yahoo.co.jp'], mail.to
+    assert_equal ['rytr823@gmail.com'], mail.from
+    # assert_match 'Hi', mail.body.encoded
   end
 end
