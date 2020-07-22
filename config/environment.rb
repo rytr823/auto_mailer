b617738fe8d79bba8cf6a7a5e1ec73191c1ecafa
rytr823 <rytr823@gmail.com>
2020-07-22 15:53:46 +0900

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'], # Send Gridから送られてきているユーザ名
   :password => ENV['SENDGRID_PASSWORD'], # ユーザ名に対応するパスワード
-  :domain => 'rytr823@gmai.com', # Send Grid登録時に設定した各自のドメイン名
+  :domain => 'heroku.com', # Send Grid登録時に設定した各自のドメイン名
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
