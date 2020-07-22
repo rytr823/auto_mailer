d1f551dfa1c4fbd0503773fea1825ff35bacd079
rytr823 <rytr823@gmail.com>
2020-07-22 14:28:09 +0900

    Edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   
     config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
     config.action_mailer.default_url_options = { host: 'shrouded-garden-96996.herokuapp.com' }
     ActionMailer::Base.smtp_settings = {
       address: 'smtp.sendgrid.net',
