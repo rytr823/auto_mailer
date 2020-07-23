bce1a79ad597ed28d542ad7d451d3391defc87bb
rytr823 <rytr823@gmail.com>
2020-07-23 16:28:45 +0900

    Update user_mailer_spec.rb

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -13,7 +13,7 @@ describe UserMailer, type: :mailer do
 
     context "when trends(user)" do
       it {expect(mail.from.first).to eq(ENV['EMAIL_HIDDEN'])}
-      it {expect(mail.to.first).to eq('rh_mc_bb@yahoo.co.jp')}
+      it {expect(mail.to.first).to eq(ENV['YAHOO_MAIL'])}
       it {expect(mail.subject).tp eq("今月の業界動向")}
     end
   end
